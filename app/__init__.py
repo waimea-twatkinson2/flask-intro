cbc79cec8f069bbebb9f80f4455b79fb29102a8e
waimea-twatkinson2 <twatkinson2@waimea.school.nz>
2025-05-19 10:58:41 +1200

    dfggfdsg

--- app/__init__.py
+++ app/__init__.py
@@ -9,10 +9,10 @@ app = Flask(__name__)
 def home():
     return render_template('pages/home.jinja')
 
-@app.get("/random/")
-def random():
-    return str(randint(1, 1000))
-
 @app.get("/about/")
 def about():
     return render_template('pages/about.jinja')
+
+@app.get("/random/")
+def random():
+    return render_template('pages/random.jinja')

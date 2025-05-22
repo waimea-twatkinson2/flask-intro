de39dd0d0b447eb3974a3ca010b2f2908b707f6c
waimea-twatkinson2 <twatkinson2@waimea.school.nz>
2025-05-22 12:23:52 +1200

    created some new pages and a form

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import redirect
 from random import randint
 from flask import render_template
 
@@ -15,4 +16,18 @@ def about():
 
 @app.get("/random/")
 def random():
-    return render_template('pages/random.jinja')
+    randNum = randint(1, 100000000000000000)
+    return render_template('pages/random.jinja' , number = randNum)
+
+@app.get("/number/<int:num>")
+def analyseNumber(num):
+    print(f"You have entered the number {num}")
+    return render_template('pages/number.jinja', number = num)
+
+@app.get("/form/")
+def form():
+    return render_template('pages/form.jinja')
+
+@app.errorhandler(404)
+def notFound(e):
+    return render_template('pages/404.jinja'), 404
